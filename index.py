8307a1a55a8e7714f47dcae8ccf39cadfb005c17
diegomza <combe202@gmail.com>
2023-05-12 22:45:48 -0300

    primeras variables con python

---
+++ index.py
@@ -0,0 +1,24 @@
+#Ejercicio N°1
+
+nomb = "Diego"
+apellido = "Combe"
+Edad = 40
+Altura = 1.80
+peso = 80
+
+print("Mi nombre completo es: ", nomb, apellido)
+print("Mi edad es: ", Edad, "años")
+print("Mi altura y peso son: ", Altura, "mts", peso, "kg")
+
+#Ejercicio N°2
+
+num = 7
+num2 = 12
+num3 = 8
+
+calculo = ((((num + 10)*5) + (num2 - 5)) + num3)/4
+
+
+print("El resultado del calculo es: ", calculo)
+
+
